a833450c3bbce71b42f84d323be7065662dc8052
YimianFang <startop@sjtu.edu.cn>
2023-07-23 13:15:06 +0800

    update

--- LSQPlus_yKL_reGDN/LSQPlus_yKL_reGDN_model.py
+++ LSQPlus_yKL_reGDN/LSQPlus_yKL_reGDN_model.py
@@ -40,7 +40,22 @@ class LSQPlus_reGDN_ScaleHyperprior(CompressionModel):
             LSQPlusConv2d(fpmodel.g_a[6], signed=True),
         )
 
-        self.g_a_fp = copy.deepcopy(fpmodel.g_a)
+        # self.g_a_fp = copy.deepcopy(fpmodel.g_a)
+        
+        self.g_a_fp = nn.Sequential(
+            conv(3, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, M),
+        )
+        
+        self.g_a_fp[0].weight = fpmodel.g_a[0].weight
+        self.g_a_fp[2].weight = fpmodel.g_a[2].weight
+        self.g_a_fp[4].weight = fpmodel.g_a[4].weight
+        self.g_a_fp[6].weight = fpmodel.g_a[6].weight
 
         self.g_s = nn.Sequential(
             LSQPlusConvTranspose2d(fpmodel.g_s[0], signed=True),
